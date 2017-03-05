3d43956a407c0dcfe51294e2a5b96f4be2be05ba
po <shdyh2003@gmail.com>
2017-03-06 00:38:44 +0900

    更新メソッドと描画メソッド追加
    ついでに赤丸を描画

--- App.py
+++ App.py
@@ -3,8 +3,12 @@ from pygame.locals import *
 import sys
 
 class App:
-	screen = None
+	surface = None # サーフェイス
 	clearColor = (0,0,255) # クリアカラー.
+	handlerUpdate = None # 更新コールバック関数
+	handlerDraw = None # 描画コールバック関数
+	clock = pygame.time.Clock() # フレームレート制御.
+	frameRate = 60 # フレームレート.
 
 	# コンストラクタ.
 	def __init__(self):
@@ -14,20 +18,25 @@ class App:
 	def init(self, caption, size):
 		# 初期化.
 		pygame.init()
-		self.screen = pygame.display.set_mode(size)
+		self.surface = pygame.display.set_mode(size)
 		pygame.display.set_caption(caption) # キャプション.
 
 	# 起動.
 	def launch(self):
-		if self.screen is None:
+		if self.surface is None:
 			# no initialize?
 			print("no initilize")
 			return
 
 		while True:
 			# 塗りつぶし.
-			self.screen.fill(self.clearColor)
-			# 画面更新.
+			self.surface.fill(self.clearColor)
+			# 更新
+			if self.handlerUpdate is not None:
+				self.handlerUpdate()
+			# 描画.
+			if self.handlerDraw is not None:
+				self.handlerDraw()
 			pygame.display.update()
 
 			# イベントハンドリング.
@@ -35,8 +44,25 @@ class App:
 				# 終了イベント.
 				if event.type == QUIT:
 					sys.exit()
+			# wait
+			self.clock.tick(self.frameRate)
+
+	# フレームレート設定
+	def setFrameRate(self, frameRate):
+		self.frameRate = frameRate
 
 	# クリアカラー設定.
 	def setClearColor(self, color):
 		self.clearColor = color
 
+	# 更新メソッドのコールバック設定.
+	def setHandlerUpdate(self, func):
+		self.handlerUpdate = func
+
+	# 描画メソッドのコールバック設定.
+	def setHandlerDraw(self, func):
+		self.handlerDraw = func
+
+	# 円を描画.
+	def drawCircle(self, x, y, radius, color):
+		pygame.draw.circle(self.surface, color, (x, y), radius)
--- main.py
+++ main.py
@@ -1,14 +1,71 @@
 import pygame
 from pygame.locals import * 
 import sys
+import random
 import App
 
 # 定数.
 SCREEN_CAPTION = u"My Pygame Application"
-SCREEN_SIZE = (640, 480) # 画面サイズ.
+SCREEN_WIDTH = 640 # 画面幅
+SCREEN_HEIGHT = 480 # 画面高さ
 SCREEN_COLOR = (0,0,0) # 画面カラー.
+FRAME_RATE = 60	# フレームレート.
+# 色定数
+COLOR_BLACK		= (0,0,0) # 黒
+COLOR_WHITE		= (255,255,255) # 白
+COLOR_RED		= (255,0,0) # 赤
+COLOR_BLUE		= (0,0,255) # 青
+COLOR_GREEN		= (0,255,0) # 緑
+COLOR_CYAN		= (0,255,255) # シアン
+COLOR_YEWLLOW	= (255,255,0) # 黄
+COLOR_MAGENTA	= (255,0,255) # マゼンタ
 
-app = App.App()
-app.init(SCREEN_CAPTION, SCREEN_SIZE)
-app.setClearColor(SCREEN_COLOR)
-app.launch()
+# 変数
+app = None
+# TEST:
+x = 0
+y = 0
+speedX = 8
+speedY = 3
+
+# 初期化
+def init():
+	global app
+	app = App.App()
+	app.init(SCREEN_CAPTION, (SCREEN_WIDTH, SCREEN_HEIGHT))
+	app.setClearColor(SCREEN_COLOR) # クリアカラー.
+	app.setHandlerUpdate(update)
+	app.setHandlerDraw(draw)
+	app.setFrameRate(FRAME_RATE)
+	app.launch() # 起動.
+
+# 更新.
+def update():
+	if (app is None):
+		return
+
+	# TEST:
+	global x
+	global y
+	global speedX
+	global speedY
+
+	tx = x + speedX
+	ty = y + speedY
+	if ( tx < 0 or tx > SCREEN_WIDTH):
+		speedX = int(speedX * (random.random() + 0.5) * -1)
+		tx = x + speedX
+	if ( ty < 0 or ty > SCREEN_HEIGHT):
+		speedY = int(speedY * (random.random() + 0.5) * -1)
+		ty = y + speedY
+	x = tx
+	y = ty
+
+# 描画.
+def draw():
+	# TEST:
+	global x
+	global y
+	app.drawCircle(x, y, 30, COLOR_RED)
+	
+init()
